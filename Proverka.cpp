de2e4ad77cedea607a39394246e87cbb8620514a
muvte <dtroynev@mail.ru>
2018-02-05 17:07:04 +0300

    Proverka

--- Proverka.cpp
+++ Proverka.cpp
@@ -18,6 +18,7 @@ double f3 (double x)
 {
 	return cos(x) * x;
 }
+
 double f4 (double x)
 {
 	return x * x;
@@ -30,24 +31,26 @@ double f5 (double x)
 
 double gradient (double(*pfunc)(double), double x0, double dx)
 {
-	double p;
-	p = ((*pfunc)(x0 + dx) - (*pfunc)(x0 - dx))/(2 * dx);
-	return p;
+	return ((*pfunc)(x0 + dx) - (*pfunc)(x0 - dx))/(2 * dx);
 }
 
 int main () {
 	setlocale (0,"");
 	int i, a, x0;
 	double p, l;
-	int const N = 1000;  
+	int const N = 1000;
 	double const pi = 3.1416; 
 	double const dx = 0.001;
 	
-	double x1 = -pi*3;   
-	double x2 = pi*3;  
+	double x1;   
+	double x2; 
+	cout << "Поставьте границы функции x1 и x2" << "\n" << "x1 = ";
+	cin >> x1;
+	cout << "x2 = ";
+	cin >> x2;
 	
-	double X[N];  
-	double Y[N];  
+	double X[N];
+	double Y[N];
 	double P[N];
 	cout << "1.y=sin(x) 2.y=sin(2x) 3.y=x*cos(x) 4.y=x^2 5.y=x^3" << "\n";
 	cout << "Выберите функцию." << "\n";
@@ -56,30 +59,22 @@ int main () {
 	if (a == 1)  // Смотрим к какой фукции подходит а
 		pfunc = f1;
 	if (a == 2) 
-		pfunc = f2;
-	if (a == 3)
+		pfunc = f2;if (a == 3)
 		pfunc = f3;
 	if (a == 4) 
 		pfunc = f4;
 	if (a == 5)
 		pfunc = f5;
-	for (int i = 0; i < N; i++) { // Строим массив точек абсцисс функции
+	for (i = 0; i < N; i++) { // Строим массив точек абсцисс функции
 		X[i] = x1 + (x2 - x1)/(N-1)*i;
 	}
-	for (int i = 0; i < N; i++) { // Строим массив точек ординат функции и ординат производной
-		Y[i] = (*pfunc)(X[i]);
-<<<<<<< HEAD
-		P[i] = gradient(pfunc, x0, dx);
-=======
-		P[i] = 
-		
->>>>>>> 919a551ba5f3eb725372286be12c11b9691b0268
-	}
+	cout << (*pfunc)(X[1]);
+	cout << ((*pfunc)(X[1] + dx) - (*pfunc)(X[1] - dx))/(2 * dx);
 	
-	cout << "На какой абсциссе посчитать производную?" << "\n";;
-	cin >> x0;
-	p = gradient(pfunc, x0, dx);
-	cout << p;
+	for (i = 0; i < N; i++) { // Строим массив точек ординат функции и ординат производной
+		Y[i] = (*pfunc)(X[i]);
+		P[i] = ((*pfunc)(X[i] + dx) - (*pfunc)(X[i] - dx))/(2 * dx);
+	}
 	
 	ofstream datafile1;
 	datafile1.open ("data1.txt");
@@ -91,7 +86,7 @@ int main () {
 	ofstream datafile2; // Записываем в файл точку, для выделения места в котором вычислили производную
 	datafile2.open ("data2.txt");
 	for (i = 0; i < N; i++) {
-		datafile2 << X[i] << "\t" << Y[i] << "\n";
+		datafile2 << X[i] << "\t" << P[i] << "\n";
 	}
 	datafile2.close();
 	
