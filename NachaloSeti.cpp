9a3799c564733e06e809c8fbb5b83b9d959473aa
muvte <dtroynev@mail.ru>
2018-02-25 23:44:00 +0300

    Updated commit shagi added

--- NachaloSeti.cpp
+++ NachaloSeti.cpp
@@ -35,14 +35,16 @@ double gradient (double(*pfunc)(double), double x0, double dx)
 	return ((*pfunc)(x0 + dx) - (*pfunc)(x0 - dx))/(2 * dx); //Геометрический смысл производной tg a 
 }
 
-double spusk (double(*pfunc)(double), double x0, double dx, double A, int x1, int x2, double alt, int naprav)
+double spusk (double(*pfunc)(double), double x0, double dx, double A, int x1, int x2, int naprav, int OPRED, int shagi)
 {
 	double h, epsilon; // h - шаг, epsilon - допустимая погрешность поиска экстремумма
 	ofstream datafile2; // Записываем в файл точки, по которым спускались
 	datafile2.open ("data2.txt");
 	h = 1; //Вводим любой шаг, чтобы в нем не лежал мусор
 	epsilon = 5 * dx;
+	shagi = 0;
 	while (x0 > x1 && x0 < x2) {
+		shagi++;
 		datafile2 << x0 << "\t" << (*pfunc)(x0) << "\n"; //Записываем в файл точки движения
 		if (naprav == 1) {
 			h = - (gradient (*pfunc, x0, dx)) * A; 
@@ -51,16 +53,19 @@ double spusk (double(*pfunc)(double), double x0, double dx, double A, int x1, in
 		}
 		x0 = x0 + h;
 		if (h < epsilon && h > -epsilon && gradient (*pfunc, x0 - 1, dx) * gradient (*pfunc, x0 + 1, dx) < 0) { //Если шаг из промежутка (-epsilon,epsilon), то мы находимся в погрешности от точки экстремумма
+			cout << "Количесто шагов при нахождении экстрэмума = " << shagi << "\n";
 			return x0; // Отступаем влево и вправо от экстрэмума и ищем там производные. Если они разных знаков, то при умножении друг на друга результат будет < 0
 		}
 		if (x0 <= x1) {
-			alt = 404;
+			OPRED = 0;
 			datafile2 << x1 << "\t" << (*pfunc)(x1) << "\n"; //Записываем в файл точку края ф-ции
+			cout << "Количесто шагов при нахождении экстрэмума = " << shagi << "\n";
 			return x1;
 		}
 		if (x0 >= x2) {
-			alt = 404;
+			OPRED = 0;
 			datafile2 << x2 << "\t" << (*pfunc)(x2) << "\n"; //Записываем в файл точку края ф-ции
+			cout << "Количесто шагов при нахождении экстрэмума = " << shagi << "\n";
 			return x2;
 		}
 	}
@@ -69,8 +74,8 @@ double spusk (double(*pfunc)(double), double x0, double dx, double A, int x1, in
 
 int main () {
 	setlocale (0,"");
-	int i, a, b, naprav; // i - счетчик, a - выбор функции, b - коэф продолжения работы, naprav - что будем искать min/max
-	double x0, xGrad, A, alt; // x0 - точка старта, xGrad - точка минимума максимума, A - коэфициент спуска, alt - альтернативный экстрэмум
+	int i, a, b, naprav, OPRED, alt4, shagi; // i - счетчик, a - выбор функции, b - коэф продолжения работы, naprav - что будем искать min/max, alt4 - экстрэмум параболы, shagi - кол-во шагов градиента
+	double x0, xGrad, A, l, alt; // x0 - точка старта, xGrad - точка минимума максимума, A - коэфициент спуска, alt - альтернативный экстрэмум
 	int const N = 1000; // кол-во точек функции
 	double const dx = 0.001; //шаг
 	b = 1; // Вводим конкретное значение для зацикливания программы
@@ -96,23 +101,29 @@ int main () {
 		double(*pfunc)(double); // Объявляем указатель
 		if (a == 1) { // Смотрим к какой фукции подходит а
 			pfunc = f1;
-			alt = 404; // Задаем альтернативному экстрэмуму значение ошибки, чтобы погрешность не выдавалась
+			OPRED = 1;
+			l = - pi / 2 + pi * -50; //Тк меньше -50 я думаю мы вводить не будем
+			while (!(x0 >= l && x0 <= l + pi)) { // Ищем промежуток от pi / 2 * n до pi / 2 * n + pi
+				l = l + pi;
+			}
+			if (naprav == 1) { //Если спуск, то выбираем нижнюю точку промежутка
+				alt = l + pi;
+			} else {
+				alt = l; //Если подъем, то выбираем верхнюю точку промежутка
+			}			
 		}
 		if (a == 2) {
 			pfunc = f2;
-			alt = 404;
 		}
 		if (a == 3) {
 			pfunc = f3;
-			alt = 404;
 		}
 		if (a == 4) {
 			pfunc = f4;
-			alt = 0;
+			OPRED = 1;
 		}
 		if (a == 5) {
 			pfunc = f5;
-			alt = 404;
 		}
 		for (i = 0; i < N; i++) { // Строим массив точек абсцисс функции
 			X[i] = x1 + (x2 - x1)/(N-1)*i;
@@ -123,11 +134,15 @@ int main () {
 
 		cout << "Введите коэфициент спуска А, то есть чем А больше, тем больше шаг" << "\n" << "Для функций y=x^3 и y=x*cos(x) совесуется выбирать A < 0.1" << "\n";
 		cin >> A;
-		xGrad = spusk(*pfunc, x0, dx, A, x1, x2, alt, naprav); //Пишем в xGrad абсциссу точки экстремумма
+		xGrad = spusk(*pfunc, x0, dx, A, x1, x2, naprav, OPRED, shagi); //Пишем в xGrad абсциссу точки экстремумма
 		cout << "Абцисса экстрэмума = " << xGrad << "\n";
-		if (alt != 404) {
-			cout << "Погрешность = " << abs(alt - xGrad) << "\n"; //Берем модуль от погрешности, так как она всегда положительная
-		} 
+		alt4 = xGrad; // alt4 определен типом int так что при параболе (a = 4) альтернативный экстрэмум будет вычислыться точно
+		if (a == 4) {
+			alt = alt4;
+		}
+		if (OPRED == 1) {
+			cout << "Погрешность = " << abs(abs(alt) - abs(xGrad)) << "\n"; //Берем модуль от погрешности, так как она всегда положительная
+		}
 
 		ofstream datafile1;
 		datafile1.open ("data1.txt");
