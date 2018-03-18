2ab48b7f5c4ef84119bcb94739d345e35dac80d3
muvte <dtroynev@mail.ru>
2018-03-18 20:18:15 +0300

    18.03.2018

--- PostroenieOtDvuhPeremennih.cpp
+++ PostroenieOtDvuhPeremennih.cpp
@@ -24,10 +24,10 @@ void gradient (double X0 [], double Xout[], int n, double dx)
 		Xout[i] = (f(X1) - f(X2)) / (2 * dx); 
 	}
 }
-void spusk (double X0 [], double Xout[], int n, double dx)
+void spusk (double X0 [], double Xout[], int n, double dx, int* shagi)
 {
 	setlocale (0,"");
-	double x1, x2, epsilon, A; //x1, x2 - координаты старта, epsilon - окресность обнаружения экстрэмума, А - коэф спуска
+	double x1, x2, epsilon, A, c = 0; //x1, x2 - координаты старта, epsilon - окресность обнаружения экстрэмума, А - коэф спуска, c - счетчик
 	epsilon = 5 * dx;
 	cout << "Введите координаты x1 и x2 с которых будет начинаться спуск \n";
 	cin >> x1 >> x2;
@@ -42,6 +42,7 @@ void spusk (double X0 [], double Xout[], int n, double dx)
 	datafile2.open ("data2.txt");
 	while (1) {
 		datafile2 << x1 << "\t" << x2 << "\t" << f(X1) << "\n"; //Записываем в файл точки движения
+		c++;
 		x1 = x1 - A * Xout[0]; //Шагаем
 		x2 = x2 - A * Xout[1];
 		X1[0] = x1; // Заполняем вектор *старта* уже новыми значениями
@@ -52,6 +53,7 @@ void spusk (double X0 [], double Xout[], int n, double dx)
 			Xout[0] = x1; // Заполняем вектор *старта* уже новыми значениями
 			Xout[1] = x2;
 			Xout[2] = f(Xout);
+			*shagi = c; //Передаем в указатель на итоговую переменную кол-ва шагов
 			break;
 		}
 	}
@@ -59,26 +61,39 @@ void spusk (double X0 [], double Xout[], int n, double dx)
 }
 int main ()
 {
-	int n = 3;
+	setlocale (0,"");
+	int i, n = 3, b = 1, shagi = 0;
 	double const dx = 0.001;
 	double X0[n-1];
 	double Xout[n-1];
-	spusk (X0, Xout, n, dx);
 	
-	ofstream datafile1;
-	datafile1.open ("data1.txt");
-	for (X0[0] = -10; X0[0] < 10; X0[0] = X0[0] + 0.1) {
-		for (X0[1] = -10; X0[1] < 10; X0[1] = X0[1] + 0.1) {
-			datafile1 << X0[0] << "\t" << X0[1] << "\t" << f(X0) << endl;
+	while (b == 1) {
+		spusk (X0, Xout, n, dx, &shagi);
+		
+		cout << "Координаты экстрэмумма - ";
+		for (i = 0; i < n; i++) {
+			cout << Xout[i] << "\t";
 		}
+		cout << endl << "Количество шагов - " << shagi << endl;
+		ofstream datafile1;
+		datafile1.open ("data1.txt");
+		for (X0[0] = -10; X0[0] < 10; X0[0] = X0[0] + 0.1) {
+			for (X0[1] = -10; X0[1] < 10; X0[1] = X0[1] + 0.1) {
+				datafile1 << X0[0] << "\t" << X0[1] << "\t" << f(X0) << endl;
+			}
+			datafile1 << endl;
+		}
+		datafile1.close();
+	
+		ofstream pl;
+		pl.open ("plot.graph");
+		pl << "set term qt" << endl;
+		pl << "splot 'data1.txt' w l, 'data2.txt' with points lw 2 lt rgb 'red'" << endl; 
+		pl << "pause mouse button1,keypress"<< endl; 
+		pl.close();
+		cout << "Закройте Gnuplot после просмотра графика для возвращения к работе." << "\n";
+		system("gnuplot plot.graph");
+		cout << "0 - Выход, 1 - Продолжить работу." << "\n";
+		cin >> b; // при b = 0 работа завершается
 	}
-	datafile1.close();
-
-	ofstream pl;
-	pl.open ("plot.graph");
-	pl << "set term qt" << endl;
-	pl << "splot 'data1.txt' w l, 'data2.txt' with points lw 2 lt rgb 'red'" << endl; 
-	pl << "pause mouse button1,keypress"<< endl; 
-	pl.close();
-	system("gnuplot plot.graph");
 }
