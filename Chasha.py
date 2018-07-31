3a3baa7d34f432ce2560224e5993f2ccba519cd4
muvte <dtroynev@mail.ru>
2018-08-01 00:22:40 +0300

    3d chasha

---
+++ Chasha.py
@@ -0,0 +1,22 @@
+import pylab
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib import cm
+import numpy
+
+def makeData ():
+    x = numpy.arange (-10, 10, 0.07)
+    y = numpy.arange (-10, 10, 0.07)
+    xgrid, ygrid = numpy.meshgrid(x, y)
+
+    zgrid = numpy.sin(pow( pow(xgrid, 2) + pow(ygrid, 2), 0.5))
+    return xgrid, ygrid, zgrid
+
+x, y, z = makeData()
+
+fig = pylab.figure()
+axes = Axes3D(fig)
+
+axes.plot_surface(x, y, z, rstride=4, cstride=4, cmap = cm.Spectral)
+
+pylab.show()
